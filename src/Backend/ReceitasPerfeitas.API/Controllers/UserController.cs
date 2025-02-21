33f072bd7bf2c2de862f57e5b0535560c246c9f3
Jefferson Viana <jefferson.vianasantos@outlook.com>
2025-02-21 08:28:18 -0300

    feat: adding endpoint register user

---
+++ src/Backend/ReceitasPerfeitas.API/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReceitasPerfeitas.Communication.Request;
+using ReceitasPerfeitas.Communication.Response;
+
+namespace ReceitasPerfeitas.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisterUserJson),StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request)
+        {
+            return Created();
+        }
+    }
+}
---
+++ src/Shared/ReceitasPerfeitas.Communication/Request/RequestRegisterUserJson.cs
@@ -0,0 +1,11 @@
+﻿ 
+
+namespace ReceitasPerfeitas.Communication.Request
+{
+    public class RequestRegisterUserJson
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
